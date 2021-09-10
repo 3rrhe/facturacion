50cef259ed58d1a2591a353ff3bb7c3e2fed2742
hroca <rroca@praga.ws>
2021-09-09 22:01:53 -0600

    First commit

---
+++ src/test/java/com/umg/rroca/billing/BillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.rroca.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
